db1123d7db1848c945cffae0ef0f08d11e22f0b2
maris-rozenstoks <146951342+maris-rozenstoks@users.noreply.github.com>
2023-11-02 12:30:05 +0200

    Scene Delegate added

--- TableViewApp/Controller/TrackTableViewController.swift
+++ TableViewApp/Controller/TrackTableViewController.swift
@@ -11,46 +11,56 @@ class TrackTableViewController: UITableViewController {
     
     var songs = Song.createSong()
     
-//    var cover = [
-//        "Carly Rae Japsen - Dedicated",
-//        "Dj Khaled - father of",
-//        "Injury Reserve - Jailbrake",
-//        "Jonas Brothers - Happiness Begins ",
-//        "Joyner Lucas - adhd",
-//        "Maluma - 11-11",
-//        "Never Young - LOSER",
-//        "Rammstein - rammstein",
-//        "Skepta - Ignorance Is Bliss",
-//        "Skillet - Victory",
-//        "Tylor the Creator - IGOR",
-//        "YG - Stop Snitching",
-//    ]
-
+    override func loadView() {
+        super.loadView()
+        print("func: \(#function), line: \(#line)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("func: \(#function), line: \(#line)")
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("func: \(#function), line: \(#line)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("func: \(#function), line: \(#line)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("func: \(#function), line: \(#line)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("func: \(#function), line: \(#line)")
+    }
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return songs.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "songCell", for: indexPath)
-
+        
         let song = songs[indexPath.row]
         cell.textLabel?.text = song.track
         cell.detailTextLabel?.text = song.album
         cell.imageView?.image = UIImage(named: song.cover)
-
+        
         return cell
     }
     
@@ -70,43 +80,38 @@ class TrackTableViewController: UITableViewController {
             detailVC.song = songs[indexPath.row]
         }
     }
-
-    /*
+    
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
-
-    /*
+    
+    
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            songs.remove(at: indexPath.row)
+            tableView.beginUpdates()
+            tableView.deleteRows(at: [indexPath], with: .bottom)
+            tableView.endUpdates()
+        }
     }
-    */
-
-    /*
+    
+    
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        let currentIndex = songs.remove(at: fromIndexPath.row)
+        songs.insert(currentIndex, at: to.row)
     }
-    */
-
-    /*
+    
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
-
-
-
-
 }
